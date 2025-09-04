55973507ab7dc0e75f8f5983200d57a03be7b127
Rodrigo U <almonacidru@MOYSP.GOV.AR>
2025-09-04 08:59:23 -0300

    css

--- PWD/vista/tp1/ej1Index.php
+++ PWD/vista/tp1/ej1Index.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ejercicio 1</title>
+    <link rel="stylesheet" href="../../CSS/general.css">
 </head>
 <body>
     <div class="contenedor">
